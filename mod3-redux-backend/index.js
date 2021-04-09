ffbadca3f655cead8d5d4e734d30036bab8ba121
Sam Gold <samg915757@gmail.com>
2021-04-09 15:50:38 -0600

    syncing up repo

--- mod3-redux-backend/index.js
+++ mod3-redux-backend/index.js
@@ -3,16 +3,18 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = 8080;
 
+
 const axios = require('axios');
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/new', (_, res) => {
   axios.get("https://deckofcardsapi.com/api/deck/new")
     .then(function({ data: newDeck }) {
-      console.log(newDeck)
       res.send(newDeck)
     })
 })
