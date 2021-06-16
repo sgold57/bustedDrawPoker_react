d453a030fb9ebc2ee3443805eba01aea58867957
Sam Gold <samg915757@gmail.com>
2021-06-15 22:40:33 -0600

    result hand separated into values and suits... hand evaluation to come

--- mod3_redux/src/Components/Card.js
+++ mod3_redux/src/Components/Card.js
@@ -9,6 +9,7 @@ export default class Card extends Component {
   render(){
     return(
       <img 
+        className="card-img"
         src={this.props.cardImage} 
         alt={this.props.cardAlt} 
         onClick={() => {this.props.handleHitClick(this.props.cardInfo, this.props.card)}}
