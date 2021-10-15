65b5d3e62e164566024e27ab55e94a69687dd2b5
Sam Gold <samg915757@gmail.com>
2021-10-15 17:15:04 -0600

    start page image added

--- mod3_redux/src/Components/GameStart.js
+++ mod3_redux/src/Components/GameStart.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import GameStartPhoto from '../Images/gameStartPhoto.png';
 import '../App.css';
 
 
@@ -7,14 +8,15 @@ export default class GameStart extends Component {
 render(){
   return (
     <div>
+        <img id='game-start-photo' src={GameStartPhoto} alt='Dual Revolvers'></img>
         <form className='game-start-form' action="/newGame">
           <label className='game-start-text'>How many hands would you like to play?</label>
-          <select name='numHands'>
+          <select className='game-start-select' name='numHands'>
               <option value='1'>1</option>
               <option value='3'>3</option>
               <option value='5'>5</option>
-            </select>
-            <input type='submit' value='Play!' />
+          </select>
+          <input className='game-start-submit' type='submit' value='Play!' />
         </form>
     </div>
   )
